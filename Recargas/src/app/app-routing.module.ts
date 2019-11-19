785528ac6ab6dd451b39cc79f074b82258cf3039
Carlos Rodriguez <crseijasnohe@gmail.com>
2019-11-19 00:43:39 -0300

    pantalla principal a medias

--- Recargas/src/app/app-routing.module.ts
+++ Recargas/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ComponentePrincipalComponent } from './componente-principal/componente-principal.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ComponentePrincipalComponent },
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{ useHash: true })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Recargas/src/app/app.module.ts
+++ Recargas/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ComponentePrincipalComponent } from './componente-principal/componente-principal.component';
+
+/** Material Componentes */
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentePrincipalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
